88c65d97d1bfaedf989c54bfa3f118a51c3fc02e
Harshath Venkatesh <harshath.venkatesh@hestabit.in>
2025-11-05 17:06:35 +0530

    Revert "commit 4 bug"
    This reverts commit 88b8c99ac8b18d769a3933e1aa3bd5a6d121b61a.

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
 console.log(hello world);
 console.log(Day 3 at hestabit);
 console.log(task on git advanced commands);
-console,log(bisect,revert,stash,cherry pick);
-console.log(currently doing task 1);
-console.log(creating 8 commits);
-console.log(with commit 4 as intentional bug);
-console.log(for git bisect simulation);
